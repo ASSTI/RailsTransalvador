03f1ecb19f58e960dc696c7508b3afd9ae2a3f7f
admin <assti@ubunturailsserver.mshome.net>
2020-05-06 21:15:03 -0300

    Adiçao de Novas Rotas

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,12 @@ Rails.application.routes.draw do
      resources :grupo
      resources :grupo_menu
      resources :usuario 
-     #rota customizada#
+     #rotas customizada#
      get 'exibe_menu', to: 'menu#exibe_menu'
+     get 'login', to: 'usuario#auth' # não usado pelo front-end,apenas para teste(segurança!!!)
+     post 'login', to: 'usuario#auth'
+     get 'login_pwd', to: 'usuario#auth_pwd' #não usado pelo front-end, apenas para teste(segurança!!!)
+     post 'login_pwd', to: 'usuario#auth_pwd'
   end
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
